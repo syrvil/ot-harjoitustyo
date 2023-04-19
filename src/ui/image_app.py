39789790f837474199d01977c7991b5c3dd20e97
Vlle Syrjäläinen <ville.syrjalainen@gmail.com>
2023-04-19 15:00:44 +0300

    näkymälogiikka

--- src/ui/image_app.py
+++ src/ui/image_app.py
@@ -4,20 +4,25 @@ from PIL import ImageTk, Image
 from services.image_manager import image_manager
 
 
+
 class ImageApp:
     def __init__(self, master):
         self.master = master
         self.images = None
         self.current_image_index = 0
+        self.current_view = None
+        self.all_images = None
+        self.searched_images = None
+        self.loaded_images = None
         self.searched_tag = None
-        self.filename = None
         self.load_images()
         self.create_widgets()
 
     def load_images(self):
         image_manager.load_images_from_file()
         # A list of Image objects
-        self.images = image_manager.get_all_images()
+        self.all_images = image_manager.get_all_images()
+        self.current_view = "All Images"
 
     def create_widgets(self):
         # Upper menu
@@ -33,36 +38,40 @@ class ImageApp:
             self.master, text="Save", command=self.save_image)
         self.save_button.grid(row=0, column=3, padx=5, pady=5)
         
+        # View
+        self.image_view = Label(self.master, text="All Images")
+        self.image_view.grid(row=1, column=0, columnspan=5, pady=5)
+
         # Image
         self.image_label = Label(self.master)
-        self.image_label.grid(row=1, column=1, columnspan=4, padx=10, pady=10)
+        self.image_label.grid(row=2, column=1, columnspan=4, padx=10, pady=10)
 
         # Lower menu    
         self.image_tags = Label(self.master, text="Tags: ")
-        self.image_tags.grid(row=2, column=0, columnspan=5, pady=5)
+        self.image_tags.grid(row=3, column=0, columnspan=5, pady=5)
 
         self.prev_button = Button(
             self.master, text="Previous", command=self.prev_image)
-        self.prev_button.grid(row=3, column=1, padx=10, pady=5)
+        self.prev_button.grid(row=4, column=1, padx=10, pady=5)
 
         self.image_order_label = Label(self.master, text="Image 1")
-        self.image_order_label.grid(row=3, column=2, padx=10, pady=5)
+        self.image_order_label.grid(row=4, column=2, padx=10, pady=5)
 
         self.next_button = Button(
             self.master, text="Next", command=self.next_image)
-        self.next_button.grid(row=3, column=3, padx=10, pady=5)
+        self.next_button.grid(row=4, column=3, padx=10, pady=5)
 
         self.add_tag_button = Button(
             self.master, text="Add Tag", command=self.add_tag)
-        self.add_tag_button.grid(row=4, column=1, padx=10, pady=5)
+        self.add_tag_button.grid(row=5, column=1, padx=10, pady=5)
 
         self.delete_tag_button = Button(
             self.master, text="Delete Tag", command=self.delete_tag)
-        self.delete_tag_button.grid(row=4, column=2, padx=10, pady=5)
+        self.delete_tag_button.grid(row=5, column=2, padx=10, pady=5)
 
         self.search_button = Button(
             self.master, text="Search", command=self.search_image)
-        self.search_button.grid(row=4, column=3, padx=10, pady=5)
+        self.search_button.grid(row=5, column=3, padx=10, pady=5)
 
         self.update_image()
 
@@ -153,8 +162,9 @@ class ImageApp:
                     "No matches", f"No images found with tag '{tag}'")
             else:
                 self.images.clear()
-                self.images = search_results
+                self.searched_images = search_results
                 self.current_image_index = 0
+                self.current_view = "Search Results"
                 self.update_image()
         tag_window.destroy()
 
@@ -165,6 +175,7 @@ class ImageApp:
         self.image_tags.config(text=tag_text)
 
     def show_all(self):
+        self.current_view = "all"
         self.images.clear()
         self.current_image_index = 0
         self.load_images()
@@ -175,15 +186,30 @@ class ImageApp:
                                           title="Select file(s)", 
                                           filetypes=(("jpg files", "*.jpg"), ("all files", "*.*")))
         if files:
-            self.images = image_manager.load_images(files) 
+            self.images.clear()
+            self.loaded_images = image_manager.load_images(files) 
             messagebox.showinfo("Image Loaded", f"Image '{files}' loaded!")
+            self.current_view = "Load Images"
+            self.current_image_index = 0
             self.update_image()
 
-
     def save_image(self):
-        pass
+        if self.current_view == "Load Images":
+            self.all_images.extend(self.loaded_images)
+            messagebox.showinfo("New Images Added!", "New images added to database!")
+        elif self.current_view == "Search Results":
+            messagebox.showinfo("Changes saved!", "Changes saved to database!")
 
     def update_image(self):
+        if self.current_view == "All Images":
+            self.images = self.all_images
+        elif self.current_view == "Search Results":
+            self.images = self.searched_images
+        elif self.current_view == "Load Images":
+            self.images = self.loaded_images
+
+        self.image_view.config(text=self.current_view.title())
+
         image = self.images[self.current_image_index].picture
         image = image.resize((400, 400), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
